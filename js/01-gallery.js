260b1c35102dd58e2d902710f5d2138c6b21292c
Jacek <105172424+JacekSmyl@users.noreply.github.com>
2022-09-27 09:58:47 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createMarkupGallery(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.addEventListener("click", onGalleryItemClick);
+
+// Create dynamic layout
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+            </div>`;
+    })
+    .join("");
+}
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryItemEl = event.target.classList.contains("gallery__image");
+  if (!isGalleryItemEl) {
+    return;
+  }
+
+  const galleryImage = event.target;
+  const galleryImageUrlBig = galleryImage.dataset.source;
+
+  onBasicLightbox(galleryImageUrlBig);
+}
+
